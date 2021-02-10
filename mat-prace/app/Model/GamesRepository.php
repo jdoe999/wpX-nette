ceb0e9d7ccb13cb3c02ab7413fb30577ac4b217f
John Doe <johndoe@email.com>
2021-02-10 14:54:14 +0100

    jj

---
+++ mat-prace/app/Model/GamesRepository.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Model;
+
+use Nette;
+use Nette\Database\Explorer;
+
+class GamesRepository
+{
+	use Nette\SmartObject;
+
+	private Nette\Database\Explorer $database;
+
+	public function __construct(Nette\Database\Explorer $database)
+	{
+		$this->database = $database;
+	}
+
+	public function getGames()
+	{
+		return $this->database->table('games')
+			->where('created_at < ', new \DateTime)
+			->order('created_at DESC');
+    }
+    
+    public function get(int $gameId) {
+        return $this->database->table('games')
+            ->get($gameId);
+    } 
+}
